f9bc2cb84700f2a6f020b385a61dac88fd68786c
Yasin3015 <yasomarei3015@gmail.com>
2025-10-09 12:05:28 +0300

    Remove Reviews Section

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -2,10 +2,10 @@ import { cn } from "@/lib/utils";
 import { Marquee } from "./ui/Marquee";
 import SparklesText from "./ui/SpaklesText";
 import { GradualSpacing } from "./ui/GradualSpacing";
-import { reviews } from "@/data";
+// import { reviews } from "@/data";
 
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
+// const firstRow = reviews.slice(0, reviews.length / 2);
+// const secondRow = reviews.slice(reviews.length / 2);
 
 const ReviewCard = ({ name, body }: { name: string; body: string }) => {
   return (
@@ -28,7 +28,7 @@ export function Testimonials() {
       id="testimonials"
       className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg md:shadow-xl"
     >
-      <GradualSpacing text="Testimonials" className="mb-5" />
+      {/* <GradualSpacing text="Testimonials" className="mb-5" />
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((review) => (
           <ReviewCard key={review.id} {...review} />
@@ -40,7 +40,7 @@ export function Testimonials() {
         ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-black-100"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-black-100"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-black-100"></div> */}
     </div>
   );
 }
